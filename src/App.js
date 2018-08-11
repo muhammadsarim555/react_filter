23a54a9944db84beaa142ac060a6069b08f60828
Muhammad Sarim <muhammadsarim555@gmail.com>
2018-08-11 17:03:14 +0500

    added work:

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import './App.css';
 class App extends Component {
   
   render() {
-    const name = "sarim"
+    const firstName = "Muhammad"
+    const name = "sarim";
+    const number = 6;
     return (
       <div className="App">
         <header className="App-header">
@@ -14,8 +16,11 @@ class App extends Component {
           <a href="https://www.google.com" target="_blank" title="Google" >Click for Google  </a>
         </header>
         <p className="App-intro">
-          Welcome To {name}.
+          Welcome To {firstName} {name}.
         </p>
+        
+        {(number ===4 ) ? console.log( "this is right") : console.log( "not right")
+      }
       </div>
     );
   }
